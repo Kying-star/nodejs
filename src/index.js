879f96ad09d959d35b2c7aeddc72d811fa76bb03
kying-star <3077006505@qq.com>
2020-12-03 21:44:11 +0800

    init:初始化项目配置分支

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: your name
+ * @Date: 2020-12-03 21:41:16
+ * @LastEditTime: 2020-12-03 21:42:42
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /static_server/src/index.js
+ */
